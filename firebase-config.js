c4b67ccb49e68637bb52b379c579836d0ba64aaf
The Lone Wolf <117017247+thelonewolf39@users.noreply.github.com>
2025-07-21 10:20:32 -0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// firebase-config.js
+// This file contains your Firebase configuration.
+// It is imported by other HTML files that need to initialize Firebase.
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA7fOi680QeULm4pRrhMIRYfiDA8cqiFhQ",
+    authDomain: "main-website-the-lone-wolf.firebaseapp.com",
+    projectId: "main-website-the-lone-wolf",
+    storageBucket: "main-website-the-lone-wolf.firebasestorage.app",
+    messagingSenderId: "41364754870",
+    appId: "1:41364754870:web:8f76584a01a0466b72f117",
+    measurementId: "G-8MXLZSXMGT"
+};
+
+export { firebaseConfig };
+// DON'T EVEN THINK ABOUT GOING INTO THE FIREBASE, IT USES AUTH TO FIND YOU AND LET ME KNOW YOU HACKED INTO MY STUFF
